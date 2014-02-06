3391f46603c3c586925b378424071c9abaa418b3
Kotori <kotori@greenskin.hopto.org>
2014-02-06 02:07:53 -0600

    Added the Mirf library to the joystick source collection. This will aid in transmitting the joystick signals to the remote.

--- joystick_controller/joystick_controller.ino
+++ joystick_controller/joystick_controller.ino
@@ -4,12 +4,18 @@
 //  for sending commands to the remote buoy down to the ROV.
 
 #include <Arduino.h>
-#include <Wire.h>
+#include <SPI.h>
+
+#include "Mirf.h"
+#include "nRF24L01.h"
+#include "MirfHardwareSpiDriver.h"
 
 #include "joystick.h"
 
-#define JOY_X_AXIS_PIN A0
-#define JOY_Y_AXIS_PIN A1
+const int JOY_X_AXIS_PIN = A0;
+const int JOY_Y_AXIS_PIN = A1;
+const int MIRF_CE_PIN = 7;
+const int MIRF_CSN_PIN = 8;
 
 const int SERIAL_BAUDRATE = 11520;
 const int MAIN_LOOP_DELAY = 10;
@@ -19,7 +25,20 @@ joystick *joy;
 void setup() {
   Serial.begin( SERIAL_BAUDRATE );
   joy = new joystick( JOY_X_AXIS_PIN, JOY_Y_AXIS_PIN );
-  //
+
+  /*
+   * Mirf Configuration
+   */
+  Mirf.cePin = MIRF_CE_PIN;
+  Mirf.csnPin = MIRF_CSN_PIN;
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+  // Set Mirf receiving address.
+  Mirf.setRADDR( (byte *) "commr" );
+  Mirf.payload = sizeof( unsigned long );
+  // Write config and power up receiver.
+  Mirf.config();
+  Serial.println("Receiver Powered!");
 }
 
 void loop() {
